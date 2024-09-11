e719af3fdb17b87943ae3d8d1f72251d5112a7b4
Julieth221 <darlycuberos17@gmail.com>
2024-09-11 16:44:50 -0500

    se agrego un archivo prueba2.py

--- prueba.py
+++ prueba.py
@@ -1,5 +1,11 @@
 import time
 print('Hello World \n')
-print('segundo reglon')
-time.sleep(10)
-print('hola')
+# print('segundo reglon')
+time.sleep(4)
+
+print(type("hola")) #esta linea imprime en consola la palabra hola
+print(type(4)) # la funcion type me dice que tipo de dato es
+print(type(5.5)) 
+"""
+
+"""

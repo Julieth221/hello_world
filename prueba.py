5616d2fc7a0270bad626b978c98b783fecac6cc7
Julieth221 <darlycuberos17@gmail.com>
2024-09-13 13:33:50 -0500

    Ajuste conflicto generacion repositorio local

---
+++ prueba.py
@@ -0,0 +1,9 @@
+import time
+print('Hello World \n')
+# print('segundo reglon')
+time.sleep(4)
+
+print(type("hola")) #esta linea imprime en consola la palabra hola
+print(type(4)) # la funcion type me dice que tipo de dato es
+print(type(5.5)) 
+
